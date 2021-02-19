5bee5f215336b5c8df99413ec1aa761056a0f1cc
Hoil <31981919+Hoil2@users.noreply.github.com>
2021-02-19 17:50:58 +0900

    Add files via upload

--- WEB4-1/main.go
+++ WEB4-1/main.go
@@ -2,20 +2,34 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
 
 func uploadsHandler(w http.ResponseWriter, r *http.Request) {
-	file, header, err := r.FormFile("upload_file")
+	uploadFile, header, err := r.FormFile("upload_file")
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		fmt.Fprint(w, err)
 		return
 	}
+	defer uploadFile.Close()
 
 	dirname := "./uploads"
-	os.MkdirAll()
+	os.MkdirAll(dirname, 0777)
+	filepath := fmt.Sprintf("%s/%s", dirname, header.Filename)
+	file, err := os.Create(filepath)
+	defer file.Close()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, err)
+		return
+	}
+
+	io.Copy(file, uploadFile)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, filepath)
 }
 
 func main() {
@@ -23,5 +37,4 @@ func main() {
 	http.Handle("/", http.FileServer(http.Dir("public")))
 
 	http.ListenAndServe(":3000", nil)
-
 }
