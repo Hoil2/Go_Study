55566c03a1b7f33682515b3f930ff2358ec0beb0
Hoil <31981919+Hoil2@users.noreply.github.com>
2021-02-22 16:55:37 +0900

    JSON Transfer

---
+++ WEB2/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type User struct {
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type fooHandler struct{}
+
+func (f *fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	user := new(User)
+	err := json.NewDecoder(r.Body).Decode(user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, err)
+		return
+	}
+	user.CreatedAt = time.Now()
+
+	data, _ := json.Marshal(user)                      
+	w.Header().Add("content-type", "application/json") // 형식이 예쁘게 나오게 하기 위해
+	w.WriteHeader((http.StatusOK))
+	fmt.Fprint(w, string(data))
+}
+
+func barHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	if name == "" {
+		name = "World"
+	}
+	fmt.Fprintf(w, "Hello %s", name)
+}
+
+func main() {
+
+	mux := http.NewServeMux() // 라우터 클래스를 만들어서 등록하는 방식
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello World")
+	})
+
+	mux.HandleFunc("/bar", barHandler)
+
+	mux.Handle("/foo", &fooHandler{})
+
+	http.ListenAndServe(":3000", mux)
+}
